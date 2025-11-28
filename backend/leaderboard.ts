9d53c11deec032c3174123136492f770adefb84b
THammarback <62379595+THammarback@users.noreply.github.com>
2025-11-28 10:56:39 +0100

    Fix database->Database reference

--- backend/leaderboard.ts
+++ backend/leaderboard.ts
@@ -1,12 +1,12 @@
 import { Database } from "./imports.ts"; 
 
 export function leaderboard(username: string, className:string) {
-    return /*HTML*/`<div class="card">${Object.entries(database).reduce<string>((acc, [currentName, {groups}])=>{
+    return /*HTML*/`<div class="card">${Object.entries(Database).reduce<string>((acc, [currentName, {groups}])=>{
     if(className in groups){
         acc += `
             <div class="user-line ${username===currentName ? "highlight" : ""}">
                 <span>${currentName}</span>
-                <span>${Object.values(database[currentName].groups[className]).filter(x => x === '⭐' || x === '✅').join('')}</span>
+                <span>${Object.values(Database[currentName].groups[className]).filter(x => x === '⭐' || x === '✅').join('')}</span>
             </div>`
     }
     return acc
