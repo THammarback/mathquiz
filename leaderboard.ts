08c4a958b837724328d94001b96e1c8de2aed560
THammarback <62379595+THammarback@users.noreply.github.com>
2025-11-28 10:49:11 +0100

    added leaderboard.ts

---
+++ leaderboard.ts
@@ -0,0 +1,14 @@
+import { Database } from "./imports.ts"; 
+
+function leaderboard(username: string, className:string) {
+    return /*HTML*/`<div class="card">${Object.entries(database).reduce<string>((acc, [currentName, {groups}])=>{
+    if(className in groups){
+        acc += `
+            <div class="user-line ${username===currentName ? "highlight" : ""}">
+                <span>${currentName}</span>
+                <span>${Object.values(database[currentName].groups[className]).filter(x => x === '⭐' || x === '✅').join('')}</span>
+            </div>`
+    }
+    return acc
+    }, "")}</div>`
+}
